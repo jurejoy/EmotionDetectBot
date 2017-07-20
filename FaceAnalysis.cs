fe023c162163c024fc5b7ba127e1ec0476e537c2
harry <jurejoy@foxmail.com>
2017-07-20 12:24:23 +0800

    add demo

---
+++ FaceAnalysis.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ReceiveAttachmentBot
+{
+    public class FaceAnalysis
+    {
+
+        private static string happinessIndex;
+        public static string faceEmotion;
+
+        public string getEmotion(string contentJson)
+        {
+            EmotionDetect(contentJson);
+            double happinessIndexf = Convert.ToDouble(happinessIndex);
+            if (happinessIndexf <= 0.2)
+            {
+                faceEmotion = "In poor mood";
+            }
+            else if (happinessIndexf <= 0.5)
+            {
+                faceEmotion = "in good mood";
+            }
+            else if (happinessIndexf <= 0.8 )
+            {
+                faceEmotion = "in happy mood";
+            }
+            else if (happinessIndexf <= 1)
+            {
+                faceEmotion = "in very happy mood";
+            }
+
+            return faceEmotion;
+        }
+
+
+        public void EmotionDetect(string contentJson)
+        {
+            
+            JArray a = JArray.Parse(contentJson);
+
+            foreach (JObject o in a.Children<JObject>())
+            {
+                foreach (JProperty p in o.Properties())
+                {
+                    string name = p.Name;
+                    if (name == "faceAttributes")
+                    {
+                        Console.WriteLine(name);
+                        JObject faceAttributes = (JObject)p.Value;
+                        foreach (JProperty p1 in faceAttributes.Properties())
+                        {
+                            string name1 = p1.Name;
+                            if (name1 == "emotion")
+                            {
+                                Console.WriteLine(name1);
+                                JObject emotion = (JObject)p1.Value;
+                                foreach (JProperty p2 in emotion.Properties())
+                                {
+                                    string name2 = p2.Name;
+                                    if (name2 == "happiness")
+                                    {
+                                        Console.WriteLine(name2);
+                                        string happiness = (String)p2.Value;
+                                        Console.WriteLine(happiness);
+                                        happinessIndex = happiness;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ face.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceiveAttachmentBot
+{
+    class Face
+    {
+        // **********************************************
+        // *** Update or verify the following values. ***
+        // **********************************************
+
+        // Replace the subscriptionKey string value with your valid subscription key.
+        const string subscriptionKey = "1aeaf4a5de9c4d828c0ff823b5899074";
+
+        // Replace or verify the region.
+        //
+        // You must use the same region in your REST API call as you used to obtain your subscription keys.
+        // For example, if you obtained your subscription keys from the westus region, replace 
+        // "westcentralus" in the URI below with "westus".
+        //
+        // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+        // a free trial subscription key, you should not need to change this region.
+        const string uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
+
+        
+
+
+        byte[] getImageFromUrl(string url)
+        {
+            System.Net.HttpWebRequest request = null;
+            System.Net.HttpWebResponse response = null;
+            byte[] b = null;
+
+            request = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(url);
+            response = (System.Net.HttpWebResponse)request.GetResponse();
+
+            if (request.HaveResponse)
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    Stream receiveStream = response.GetResponseStream();
+                    using (BinaryReader br = new BinaryReader(receiveStream))
+                    {
+                        b = br.ReadBytes(500000);
+                        br.Close();
+                    }
+                }
+            }
+
+            return b;
+        }
+
+
+
+        /// <summary>
+        /// Gets the analysis of the specified image file by using the Computer Vision REST API.
+        /// </summary>
+        /// <param name="imageFilePath">The image file.</param>
+        public async Task<string> MakeAnalysisRequest(string imageFilePath)
+        {
+            HttpClient client = new HttpClient();
+
+            // Request headers.
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // Request parameters. A third optional parameter is "details".
+            string requestParameters = "returnFaceAttributes=emotion";
+
+            // Assemble the URI for the REST API Call.
+            string uri = uriBase + "?" + requestParameters;
+
+            HttpResponseMessage response;
+
+            // Request body. Posts a locally stored JPEG image.
+            byte[] byteData = getImageFromUrl(imageFilePath);
+
+            ByteArrayContent content = new ByteArrayContent(byteData);
+
+            // This example uses content type "application/octet-stream".
+            // The other content types you can use are "application/json" and "multipart/form-data".
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+            // Execute the REST API call.
+            response = await client.PostAsync(uri, content);
+
+            // Get the JSON response.
+            return await response.Content.ReadAsStringAsync();
+
+
+
+        }
+
+
+    }
+}
